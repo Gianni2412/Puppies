edaf6610275567ed45164a50d6cc2c39a04e5512
Aylin Santa Cruz Vargas <aylin_santacruz@hotmail.com>
2021-05-02 23:46:17 -0500

    Aplicativo web

---
+++ PuppiesPet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PuppiesPet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
