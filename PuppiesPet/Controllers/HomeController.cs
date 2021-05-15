83e4eab1573b35a58216730deb1bae3bcb2108db
Gianni caycho <caycho1797giannina@gmail.com>
2021-05-15 00:09:35 -0500

    modificaciones en el main

--- PuppiesPet/Controllers/HomeController.cs
+++ PuppiesPet/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace PuppiesPet.Controllers
             return View();
         }
 
+        public IActionResult Trabaja()
+        {
+            return View();
+        }
+
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
