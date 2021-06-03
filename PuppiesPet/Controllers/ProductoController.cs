e4b24abd1ae4e4b130c03a0eec67554b4c20173d
Aylin Santa Cruz Vargas <aylin_santacruz@hotmail.com>
2021-06-03 00:40:41 -0500

    Registros de Mascotas y listas

---
+++ PuppiesPet/Controllers/ProductoController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PuppiesPet.Data;
+using PuppiesPet.Models;
+
+namespace PuppiesPet.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult RegistarProducto()
+        {
+        return View();
+        }  
+
+    }
+
+}
