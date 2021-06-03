4478ba51d3043a70de6098fab88a5ad4cd10166c
Aylin Santa Cruz Vargas <aylin_santacruz@hotmail.com>
2021-06-02 21:07:06 -0500

    .

--- PuppiesPet/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ PuppiesPet/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -52,7 +52,7 @@ namespace PuppiesPet.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Recuérdame?")]
             public bool RememberMe { get; set; }
         }
 
